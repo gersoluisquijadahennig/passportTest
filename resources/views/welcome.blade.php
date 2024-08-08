27f849dd33a7c8d5b78bfa23d8048da46f61006f
gersoluisquijadahennig@gmail.com <gersoluisquijadahennig@gmail.com>
2024-08-07 23:24:55 -0400

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
             <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
                 <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
                     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-                        <h1> Single Sing On SSbiobio Oauth2 Passport laravel</h1>
+                        <h1> Single Sing On SSbiobio Oauth2 Passport laravel 11</h1>
                         @if (Route::has('login'))
                             <nav class="-mx-3 flex flex-1 justify-end">
                                 @auth
